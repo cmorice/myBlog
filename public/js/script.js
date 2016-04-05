3441e1ebe64fb1acf7745b7b7107e32cdefb8e9c
Charlotte Morice <cmorice.ext@orange.com>
2016-04-05 17:52:42 +0200

    Ajout de la section parente

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,17 @@
     });
 })();
 
+$(document).ready(function(){												
+
+       //Navigation Menu Slider
+        $('#nav-expander').on('click',function(e){
+      		e.preventDefault();
+      		$('body').toggleClass('nav-expanded');
+      	});
+      	$('#nav-close').on('click',function(e){
+      		e.preventDefault();
+      		$('body').removeClass('nav-expanded');
+      	});
+
+      });
+
